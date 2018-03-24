131d4d820c90552fedc5b8130039cde1f323d758
Herve Abdi <herve@utdallas.edu>
2018-03-24 14:43:05 -0500

    Put Back PTCA4CATA.R (to get index)

---
+++ R/PTCA4CATA.R
@@ -0,0 +1,40 @@
+#
+# You can learn more about package authoring with RStudio at:
+#
+#   http://r-pkgs.had.co.nz/
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Build and Reload Package:  'Cmd + Shift + B'
+#   Check Package:             'Cmd + Shift + E'
+#   Test Package:              'Cmd + Shift + T'
+# library(roxygen2)
+# library(devtools)
+# setwd('~/Dropbox/SortingWithDistatisR/ptca/PTCA4CATA/R')
+# document('PTCA4CATA')
+# roxygenise('PTCA4CATA')
+# check('PTCA4CATA')
+# build('PTCA4CATA')
+# install.packages('~/Dropbox/SortingWithDistatisR/ptca/PTCA4CATA_0.1.0.tar.gz',repos = NULL)
+# library('PTCA4CATA')
+# help(PTCA4CATA)
+# Documentation with roxygen2
+#' @title
+#' PTCA4CATA  package to perform Partial Triadic Correspondence Analysis
+#' (PTCA) for Check All That Apply (CATA) Data
+#'
+#'@description
+#' The PTCA4CATA package provides a set of tools to 1) analyze
+#' "Check All That Apply (CATA)" Data;
+#'    2) display the results graphically;
+#' and 3)
+#' perform the correct inferences.
+#' In addition to correspondence analysis, Hellinger analysis
+#' can also be used.
+#' PTCA4CATA uses Partial Triadic Correspondence Analysis (PTCA)
+#' to analyze the
+#' pseudo-contingency table obtained from a CATA task.
+#'@author Herve Abdi \email{herve@utdallas.edu}
+#'@docType package
+#'@name PTCA4CATA
+NULL
