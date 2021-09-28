5a4b2c56e9a3977dea44813416ba640d2a1d4cd2
Herve Abdi <herve@utdallas.edu>
2021-09-28 17:16:48 -0500

    fix prettyBarPlot2 ggplot2 warning "guides()"

--- R/PrettyBarPlot2.R
+++ R/PrettyBarPlot2.R
@@ -1,12 +1,19 @@
+# Entête -----
 # functions in this file:
-#  areColors
-#  lighten
-#  PrettyBarPlot2
+###  areColors ----
+###  lighten ----
+###  PrettyBarPlot2 ----
 #_____________________________________________________________________
 # JolisPlots
 # JoliPlot: the ggplots version of prettyBar etc.
 # Vincent Guillemot and Hervé Abdi
 # Created September 14, 2018
+# Last Changes Vincent G. & Hervé A.
+# September 22, 2021.
+# fix the warning
+#  guides(<scale> = FALSE)` is deprecated.
+#  Please use `guides(<scale> = "none")`
+#
 #_____________________________________________________________________
 # Helper for roxygen2 ----
 #  install.packages('sinew')
@@ -16,10 +23,14 @@
 #_____________________________________________________________________
 # areColors ----
 # Credit : https://stackoverflow.com/questions/13289009/check-if-character-string-is-a-valid-color-representation
-#' @title Check that color names are the names of some real colors.
-#' @description  \code{areColors}: Check that colores names are the names of some real colors.
-#' @param x a string vectors with (to be checked) names of colors.
-#' @return a logical vector with \code{TRUE} if a color, \code{FALSE} if not.
+#' @title Check that color names are
+#' the names of some real colors.
+#' @description  \code{areColors}: Check that color
+#' names are the names of some real colors.
+#' @param x a string vectors with (to be checked)
+#' names of colors.
+#' @return a logical vector with
+#' \code{TRUE} if a color, \code{FALSE} if not.
 #' @details credit:
 #' \url{Credit: https://stackoverflow.com/questions/13289009/check-if-character-string-is-a-valid-color-representation}
 #' @examples
@@ -49,10 +60,12 @@ areColors <- function(x) {
 # Credit : @Roland, https://stackoverflow.com/questions/30219738/is-there-a-way-to-programmatically-darken-the-color-given-rgb-values
 
 #' @title gives a lighter shade of a color
-#' @description \code{lighten} gives a lighter shade of a color.
+#' @description \code{lighten} gives a
+#' lighter shade of a color.
 #' @param colors a vector of color names.
 #' @param   factor (Default: 1.4) the lightening factor.
-#' @param   maxColorValue (Default: 255) the maximum possible for a color component.
+#' @param   maxColorValue (Default: 255)
+#'  the maximum possible for a color component.
 #' @return  A lightened color.
 #' @details
 #'  Credit to Roland, see:
@@ -82,94 +95,141 @@ lighten <- function(colors,
 #  sinew::makeOxygen(PrettyBarPlot2)
 #_____________________________________________________________________
 # PrettyBarPlot2 preamble ----
-#' @title a \code{ggplot2}-based function to plot bar plots
+#' @title a \code{ggplot2}-based
+#' function to plot bar plots
 #' for a series
 #' of variables:
-#' Used for plotting bootstrap ratios or contributions for
+#' Used for plotting bootstrap ratios
+#' or contributions for
 #' PCA, CA, MCA, PLS, etc.
 #'
 #' @description \code{PrettyBarPlot2}:
-#' a \code{ggplot2}-based version of \code{PrettybarPlot} and
+#' a \code{ggplot2}-based version of
+#' \code{PrettybarPlot} and
 #' \code{PrettyBarPlotColor}.
 #' \code{PrettyBarPlot2}
 #' creates bar plots for a set of items
 #' (e.g., observations or variables) analyzed
 #' with multivariate methods
 #' such as PCA, CA, MCA, PLS etc.
-#' \code{PrettyBarPlot2} is used to display the bootstrap ratios
+#' \code{PrettyBarPlot2}
+#' is used to display the bootstrap ratios
 #' or contributions plots for CA/MCA/PCA/PLS.
 #' Significant or important items are plotted
 #' in color, non-significant (or un-important)
 #' items are plotted in gray.
 #'
-#' @details Note that this version is still in
-#' development. Current version generates a (strange) warning message
+#' @details Note that this version
+#' is still in
+#' development. Current version generates
+#' a (strange) warning message
 #' from \code{ggplot2}
 #' about \code{element_text()}.
 
-#' @param bootratio the bootstrap ratios (BR) or contributions
+#' @param bootratio
+#' the bootstrap ratios
+#' (BR) or contributions
 #' or similar statistics to be plotted.
 #' (e.g., obtained from \code{Boot4PTCA}).
-#' @param font.size (Default: \code{NULL}) the font size to write
-#' the name of the item. When \code{NULL, font.size = 1}.
-#' @param threshold  (Default: \code{2})
+#' @param font.size
+#' (Default: \code{NULL})
+#' the font size to write
+#' the name of the item. When \code{NULL,
+#' font.size = 1}.
+#' @param threshold
+#' (Default: \code{2})
 #' The critical value for significance
 #'  which matches a \eqn{p} < .05
-#' significance level. The |BR| < threshold are plotted in gray,
+#' significance level.
+#' The |BR| < threshold are plotted in gray,
 #' the other BR are plotted in colors.
-#' @param ylim (Default: NULL)
-#' a 2-element vector giving min and max for the y-axis;
-#' when NULL (default) set to c(-max(abs(y)), max(abs(y)))
-#' @param color.bar a 3-element vector
+#' @param ylim
+#' (Default: \code{NULL})
+#' a 2-element vector giving \code{min} and
+#' \code{max} for the y-axis;
+#' when NULL (default) set to
+#' \code{c(-max(abs(y)), max(abs(y)))}
+#' @param color.bar
+#' a 3-element vector
 #' of color names for the bars for (respectively)
 #' significant positive,
 #' significant negative, and non-significant.
-#' Default is c('lavender','darkolivegreen3','gray90').
-#' @param color.bord The color for the border of the bars,
-#' Default: c("mediumpurple4", "darkolivegreen4", "gray75").
-#' @param color.letter a 3-element vector of color names
+#' Default is
+#' \code{c('lavender','darkolivegreen3','gray90')}.
+#' @param color.bord
+#' The color for the border of the bars,
+#' Default:
+#' \code{c("mediumpurple4", "darkolivegreen4", "gray75")}.
+#' @param color.letter
+#' a 3-element vector of color names
 #' for the names of
 #' the items for (respectively)
-#' significant positive, significant negative, and non-significant.
-#' Default is c("mediumpurple4",'darkolivegreen4','gray75')
-#' @param color4ns (Default: \code{'gray75'})
+#' significant positive, significant negative,
+#' and non-significant.
+#' Default is
+#' \code{c("mediumpurple4",'darkolivegreen4','gray75')}
+#' @param color4ns
+#' (Default: \code{'gray75'})
 #' color for the non-significant \code{bootratio}.
-#' @param color4bar (Default: NULL) a vector of color names
-#' (same dimension as \code{bootratio}). Needs to be either a name
-#' from the set \code{colors()} or in \code{hex} format.
-#' @param plotnames if TRUE (default) write the names of the items.
-#' @param main (default is \code{NULL}) a title for the graph.
-#' @param ylab (default is \code{NULL}) a label for
+#' @param color4bar
+#' (Default: \code{NULL}) a vector of color names
+#' (same dimension as \code{bootratio}).
+#' Needs to be either a name
+#' from the set \code{colors()}
+#' or in \code{hex} format.
+#' @param plotnames
+#' if code{TRUE} (default) write the names of the items.
+#' @param main
+#' (default is \code{NULL}) a title for the graph.
+#' @param ylab
+#' (default is \code{NULL}) a label for
 #' the \eqn{y} axis (i.e., BR).
 #' If \code{NULL} \code{ylab = 'Bootstrap ratios'}.
-#' @param sortValues (Default: \code{FALSE}) when \code{TRUE}
+#' @param sortValues
+#' (Default: \code{FALSE}) when \code{TRUE}
 #' sort the values to plot.
-#' @param signifOnly (Default: \code{FALSE}) when \code{TRUE}
+#' @param signifOnly
+#' (Default: \code{FALSE}) when \code{TRUE}
 #' plot only the significant values.
-#' @param angle.text  (Default: \code{90}, i.e., so slant)
+#' @param angle.text
+#' (Default: \code{90}, i.e., so slant)
 #' the "slanting factor" for the text.
-#' @param abbreviate_labels (Default: \code{FALSE}).
-#' @param make_labels_into_paragraphs (Default: \code{FALSE}).
-#' @param horizontal (Default: \code{TRUE}) when \code{TRUE},
+#' @param abbreviate_labels
+#' (Default: \code{FALSE}).
+#' @param make_labels_into_paragraphs
+#' (Default: \code{FALSE}).
+#' @param horizontal
+#' (Default: \code{TRUE}) when \code{TRUE},
 #' the plot is horizontal, when \code{FALSE} the plot is vertical.
-#' @param font.shrink (\code{default = 1}): a proportion for
+#' @param font.shrink
+#' (\code{default = 1}): a proportion for
 #'  how much the
 #' non-significant font shrinks.
-#' @param line.col (\code{Default = 'red'})
+#' @param line.col
+#' (\code{Default = 'red'})
 #' the color for significance
 #' for the critical value line.
-#' @param line.type The type of line for the critical line
+#' @param line.type
+#' The type of line for the critical line
 #' (\code{Default = 2}, a dashed line).
-#' @param line.size (\code{Default = .5 }), the thickness of the
+#' @param line.size
+#' (\code{Default = .5 }),
+#' the thickness of the
 #' critical line.
-#' @param line.alpha (\code{Default = .5 }), the transparency of the
+#' @param line.alpha
+#' (\code{Default = .5 }),
+#' the transparency of the
 #' critical line (0 = all transparent, 1 = opaque).
-#' @return A \code{ggplot2} object containg the graph
+#' @return
+#' A \code{ggplot2} object containg the graph
 #' (i.e., to be plotted with \code{print}).
-#' @details \code{PrettyBarPlot2} integrates \code{PrettyBarPlot}
+#' @details
+#' \code{PrettyBarPlot2} integrates \code{PrettyBarPlot}
 #' and \code{PrettyBarPlotColors}.
-#' @author Vincent Guillemot & Hervé Abdi
-#' @seealso \code{\link{PrettyBarPlot}}
+#' @author
+#' Vincent Guillemot & Hervé Abdi
+#' @seealso
+#' \code{\link{PrettyBarPlot}}
 #' \code{\link{PrettyBarPlotColor}}
 #' \code{\link{PrettyBarPlotColor4Q}}
 #' @examples
@@ -183,41 +243,42 @@ lighten <- function(colors,
 #' @export
 
 
-PrettyBarPlot2 <- function(bootratio,
-                           font.size = NULL,
-                           threshold = 3,
-                           ylim = NULL,
-                           color.bar = c("lavender",
-                                         "darkolivegreen3",
-                                         "gray90"),
-                           color.bord = c("mediumpurple4",
-                                          "darkolivegreen4",
-                                          "gray75"),
-                           color.letter = c("mediumpurple4",
-                                            "darkolivegreen4",
-                                            "gray75"),
-                           color4ns = "gray75",
-                           color4bar = NULL,
-                           plotnames = TRUE,
-                           main = NULL,
-                           ylab = NULL,
-                           sortValues = FALSE,
-                           signifOnly = FALSE,
-                           horizontal = TRUE,
-                           angle.text = if (horizontal) {
+PrettyBarPlot2 <- function(
+                  bootratio,
+                  font.size = NULL,
+                  threshold = 3,
+                  ylim = NULL,
+                  color.bar = c("lavender",
+                                 "darkolivegreen3",
+                                 "gray90"),
+                  color.bord = c("mediumpurple4",
+                                 "darkolivegreen4",
+                                 "gray75"),
+                  color.letter = c("mediumpurple4",
+                                   "darkolivegreen4",
+                                   "gray75"),
+                  color4ns = "gray75",
+                  color4bar = NULL,
+                  plotnames = TRUE,
+                  main = NULL,
+                  ylab = NULL,
+                  sortValues = FALSE,
+                  signifOnly = FALSE,
+                  horizontal = TRUE,
+                  angle.text = if (horizontal) {
                              90
                            } else {
                              0
                            },
-                           abbreviate_labels = FALSE,
-                           make_labels_into_paragraphs = FALSE,
-                           font.shrink = 1,
-                           line.col = 'red',
-                           line.type = 2,
-                           line.size = .5,
-                           line.alpha = .5
-                           # label.size = 1 # a future feature
-                           ) {
+                abbreviate_labels = FALSE,
+                make_labels_into_paragraphs = FALSE,
+                font.shrink = 1,
+                line.col = 'red',
+                line.type = 2,
+                line.size = .5,
+                line.alpha = .5
+ # label.size = 1 # a future feature?
+) {
 
   if (is.vector(bootratio)) {
     if (!is.null(color4bar)){color4bar <- gplots::col2hex(color4bar)}
@@ -320,7 +381,7 @@ PrettyBarPlot2 <- function(bootratio,
   #print(lesnoms)
   #print("LesNoms2Print")
   #print(LesNoms2Print)
-    dat <- data.frame(
+  dat <- data.frame(
     IDnum = factor(seq_along(LesNoms2Print)),
     ID = LesNoms2Print,
     bootratio = bootratio,
@@ -360,7 +421,7 @@ PrettyBarPlot2 <- function(bootratio,
     # fix the lim problem: Make sure that the lim is always printed. HA
     ylim = c(min(ylim[1], -threshold) , max(ylim[2], threshold))
   }
-      laLigneRouge <- geom_hline(
+  laLigneRouge <- geom_hline(
     yintercept = yint,
     col = line.col,
     alpha = line.alpha,
@@ -369,9 +430,9 @@ PrettyBarPlot2 <- function(bootratio,
   )
   #_____________________________________________________________________
   if (adapt_plot) {
-  # commented by HA
-  #  print("Inside adapt plot")
-  #  print(lafont)
+    # commented by HA
+    #  print("Inside adapt plot")
+    #  print(lafont)
     p <- ggplot(dat, aes(
       x = factor(ID, levels = ID), # from Luke original x = ID
       y = bootratio,
@@ -383,7 +444,7 @@ PrettyBarPlot2 <- function(bootratio,
       geom_hline(yintercept = 0) +
       scale_fill_manual(values = lescouleurs) +
       scale_color_manual(values = lescouleurs.bord) +
-      guides(fill = FALSE, color = FALSE) +
+      guides(fill = "none", color = "none") +
       labs(x = "", y = ylab) +
       ggtitle(main) +
       theme_bw() +
@@ -391,7 +452,10 @@ PrettyBarPlot2 <- function(bootratio,
         axis.line.y      = element_line(colour = "black", ),
         axis.ticks.x     = element_blank(),
         # axis.line.x      = element_line(colour = "black"),
-        axis.text.x      = element_text(angle = angle.text, hjust = hjust, vjust = 0.5, color = lescouleurs.bord, size = rel(font.size/3)),
+        axis.text.x      = element_text(
+          angle = angle.text, hjust = hjust,
+          vjust = 0.5, color = lescouleurs.bord,
+          size = rel(font.size/3)),
         panel.grid.major = element_blank(),
         panel.grid.minor = element_blank(),
         panel.border     = element_blank(),
@@ -423,7 +487,7 @@ PrettyBarPlot2 <- function(bootratio,
       geom_hline(yintercept = 0) +
       scale_fill_manual(values = lescouleurs) +
       scale_color_manual(values = lescouleurs.bord) +
-      guides(fill = FALSE, color = FALSE) +
+      guides(fill = "none", color = "none") +
       labs(x = "", y = ylab) +
       ggtitle(main) +
       theme_bw() +
@@ -460,7 +524,7 @@ PrettyBarPlot2 <- function(bootratio,
       geom_hline(yintercept = 0) +
       scale_fill_manual(values = lescouleurs) +
       scale_color_manual(values = lescouleurs.bord) +
-      guides(fill = FALSE, color = FALSE) +
+      guides(fill = "none", color = "none") +
       labs(x = "", y = ylab) +
       ggtitle(main) +
       theme_bw() +
